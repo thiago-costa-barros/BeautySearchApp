d9fdb915842eb3d6e80b27902ed518bff59acd0b
Thiago Costa Barros <114679335+thiago-costa-barros@users.noreply.github.com>
2024-02-08 21:33:19 -0300

    fix: add rating businessUnit_item card

--- app/_components/businessUnit_item.tsx
+++ app/_components/businessUnit_item.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { BusinessUnit } from "@prisma/client";
 import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
+import { Star, StarIcon } from "lucide-react";
 
 interface BusinessUnitItemProps {
     businessUnit: BusinessUnit;
@@ -13,6 +15,12 @@ const BusinessUnitItem = ({ businessUnit }: BusinessUnitItemProps) => {
         <Card className="min-w-[168px] max-w-[168px] rounded-2xl">
             <CardContent className="p-1">
                 <div className="h-40 w-full relative">
+                    <div className="absolute top-2 left-2 z-50">
+                        <Badge variant="secondary" className=" gap-1 flex items-center opacity-90">
+                            <StarIcon className="size-3 fill-primary text-primary" />
+                            <span>4,95</span>
+                        </Badge>
+                    </div>
                     {businessUnit.ImageUrl && (
                         <Image
                             src={businessUnit.ImageUrl}
