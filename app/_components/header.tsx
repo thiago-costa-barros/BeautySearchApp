e3344e45c18605619851c6c57284f83a7a1f90c3
Thiago Costa Barros <114679335+thiago-costa-barros@users.noreply.github.com>
2024-02-15 23:32:07 -0300

    fix: image header alt

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,20 +3,15 @@
 
 import { Card, CardContent } from "./ui/card";
 import Image from "next/image";
-import { signIn, signOut, useSession } from "next-auth/react";
 import SideMenuComponent from "./sideMenuIcon";
 
 const Header = () => {
-    const { data, status } = useSession();
-
-    const handleLogInClick = () => signIn("google");
-    const handleLogOutClick = () => signOut();
 
     return (
         <>
             <Card>
                 <CardContent className="p-5  justify-between flex items-center flex-row">
-                    <Image src="/logo_teste.png" width={120} height={45} alt={""} />
+                    <Image src="/logo_teste.png" width={120} height={45} alt="logo" />
                     <SideMenuComponent/>
                 </CardContent>
             </Card>
