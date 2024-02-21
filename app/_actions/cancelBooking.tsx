1118631c60f74b7d63b53a42c50428d7f2daff0c
Thiago Costa Barros <114679335+thiago-costa-barros@users.noreply.github.com>
2024-02-21 07:50:58 -0300

    fix: cancelBooking action revalidate path

--- app/_actions/cancelBooking.tsx
+++ app/_actions/cancelBooking.tsx
@@ -4,11 +4,10 @@ import { revalidatePath } from "next/cache";
 import { db } from "../_lib/prisma"
 
 export const cancelBooking = async (bookingId: any) => {
-    return await db.booking.delete({
+    await db.booking.delete({
         where: {
             id: bookingId,
         },
     });
+    revalidatePath("/bookings")
 };
-
-revalidatePath("/bookings")
